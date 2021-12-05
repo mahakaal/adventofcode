400fa06d5b3bd79382d32fc335fb282b5f48c8c4
Sukhdev Mohan <s.mohan@myti.it>
2021-12-05 23:19:47 +0100

    day 5, part 1 solved, part 2 not yet

---
+++ 2021/day5/day5.py
@@ -0,0 +1,57 @@
+import re
+from typing import List
+
+
+def read_file(file_name: str) -> List:
+    with open(file_name, "r") as file:
+        return [[tuple(map(lambda x: int(x), re.split(',', points))) for points in row.split("->")]
+                for row in file.read().strip().split("\n")]
+
+
+def generate_linspace(cols: int) -> List:
+    return [[0] * cols for _ in range(0, cols)]
+
+
+def get_straight_lines(lines: List) -> List:
+    return list(filter(lambda x: x[0][0] == x[1][0] or x[0][1] == x[1][1], lines))
+
+
+def part1(lines: List):
+    plane = generate_linspace(1000)
+
+    for line in lines:
+        x1, y1 = line[0]
+        x2, y2 = line[1]
+
+        if x1 == x2:
+            from_y, to_y = y1 if y1 < y2 else y2, (y1 + 1) if y1 > y2 else (y2 + 1)
+            for y in range(from_y, to_y):
+                plane[x1][y] += 1
+        elif y1 == y2:
+            from_x, to_x = x1 if x1 < x2 else x2, (x1 + 1) if x1 > x2 else (x2 + 1)
+            for x in range(from_x, to_x):
+                plane[x][y1] += 1
+        else:
+            x, y = line[0]
+            while True:
+                if x <= x2:
+                    x += 1
+                elif x >= x2:
+                    x -= 1
+
+                if y <= y2:
+                    y += 1
+                elif y >= y2:
+                    y -= 1
+
+                plane[x][y] += 1
+                # print(line, x, y, x1, y1, x2, y2)
+
+                if x == x2 and y == y2:
+                    break
+
+    return sum(elem > 1 for row in plane for elem in row)
+
+
+print("Part 1", part1(get_straight_lines(read_file("puzzle.txt"))))
+print("Part 2", part1(read_file("puzzle.txt")))
