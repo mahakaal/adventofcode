2972276e1472fdbeb1a2447e27f3657f125aecc4
Sukhdev Mohan <s.mohan@myti.it>
2021-12-30 23:36:32 +0100

    solution for day 9 part b

--- 2021/day9/day9.py
+++ 2021/day9/day9.py
@@ -24,7 +24,21 @@ def extract_window(x, y, matrix):
     return [matrix[x-1][y], matrix[x][y-1], matrix[x+1][y], matrix[x][y+1]]
 
 
-heightmap = read_file("test.txt")
+def check_basin(matrix, x, y, visited_nodes, r_len, c_len):
+    if x < 0 or y < 0 or x > r_len or y > c_len \
+            or matrix[x][y] == 9 \
+            or (x, y) in visited_nodes:
+        return
+
+    visited_nodes.append((x, y))
+
+    check_basin(matrix, x, y + 1, visited_nodes, r_len, c_len)
+    check_basin(matrix, x, y - 1, visited_nodes, r_len, c_len)
+    check_basin(matrix, x - 1, y, visited_nodes, r_len, c_len)
+    check_basin(matrix, x + 1, y, visited_nodes, r_len, c_len)
+
+
+heightmap = read_file("puzzle.txt")
 heights_sum = []
 for i in range(0, len(heightmap)):
     for j in range(0, len(heightmap[0])):
@@ -38,8 +52,9 @@ print(sum(value + 1 for _, _, value in heights_sum))
 basins = []
 for heights in heights_sum:
     i, j, z = heights
-    size = 1
+    visited = []
+    check_basin(heightmap, i, j, visited, len(heightmap) - 1, len(heightmap[0]) - 1)
+    basins.append(len(visited))
 
-    while True:
-        starting_window = extract_window(i, j, heightmap)
-        
+basins.sort(reverse=True)
+print(basins[0] * basins[1] * basins[2])
